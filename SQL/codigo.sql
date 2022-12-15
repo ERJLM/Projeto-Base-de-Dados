ec76164f2662f3e08c41e3bca0faef2fbb417ec1
ERJLM <99908664+ERJLM@users.noreply.github.com>
2022-12-15 01:41:15 +0000

    Update codigo.sql

--- SQL/codigo.sql
+++ SQL/codigo.sql
@@ -4,17 +4,36 @@ CREATE TABLE COMPANY (
   `CompanyId` int(11) NOT NULL AUTO_INCREMENT,
   `Name` varchar(255) NOT NULL,
   `IndustryId` int(11),
-  `FundsRaised` float(11,4) NOT NULL,
-  `StageId` int(11) ,
-  `LocationId` int(11) ,
-  `CountryId` int(11),
    PRIMARY KEY (`CompanyId`),
    KEY `IndustryId` (`IndustyId`),
-   KEY `StageId` (`StageId`),
-   KEY `LocationId` (`LocationID`),
-   CONSTRAINT `company_ibfk_1` FOREIGN KEY (`IndustryId`) REFERENCES `INDUSTRY` (`IndustyId`) ON UPDATE CASCADE,
-   CONSTRAINT `company_ibfk_2` FOREIGN KEY (`StageId`) REFERENCES `STAGE` (`StageId`) ON UPDATE CASCADE,
-   CONSTRAINT `company_ibfk_3` FOREIGN KEY (`LocationId`) REFERENCES `LOCATION` (`LocationId`) ON UPDATE CASCADE
+   
+   CONSTRAINT `company_ibfk_1` FOREIGN KEY (`IndustryId`) REFERENCES `INDUSTRY` (`IndustyId`) ON UPDATE CASCADE  
+  )
+
+DROP TABLE IF EXISTS `COMPANY_INDUSTRY`;
+
+CREATE TABLE COMPANY_INDUSTRY (
+  `CompanyId` int(11) NOT NULL,
+  `IndustryId` int(11)NOT NULL,
+   PRIMARY KEY (`CompanyId`),
+   PRIMARY KEY (`IndustyId`),
+   
+   CONSTRAINT `company_industry_ibfk_1` FOREIGN KEY (`CompanyId`) REFERENCES `INDUSTRY` (`CompanyId`) ON UPDATE CASCADE,
+   CONSTRAINT `company_industry_ibfk_2` FOREIGN KEY (`IndustryId`) REFERENCES `INDUSTRY` (`IndustyId`) ON UPDATE CASCADE  
+  )
+
+DROP TABLE IF EXISTS `HEADQUARTER`;
+
+CREATE TABLE HEADQUARTER (
+  `HeadquarterId` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(255) NOT NULL,
+  `CompanyId` int(11),
+  `LocationId` int(11),
+   PRIMARY KEY (`HeadquarterId`),
+   KEY `CompanyId` (`CompanyId`),
+   
+   CONSTRAINT `headquarter_ibfk_1` FOREIGN KEY (`CompanyId`) REFERENCES `COMPANY` (`CompanyId`) ON UPDATE CASCADE,
+   CONSTRAINT `headquarter_ibfk_2` FOREIGN KEY (`LocationId`) REFERENCES `LOCATION` (`LocationId`) ON UPDATE CASCADE    
   )
 
 
@@ -22,17 +41,18 @@ DROP TABLE IF EXISTS `LAYOFF`;
 
 CREATE TABLE LAYOFF(
   `LayoffId` int(11) AUTO_INCREMENT,
-  `CompanyId` int(11),
-  `LocationId` int(11) ,
+  `HeadquarterId` int(11) NOT NULL,
+  `FundsRaised` int(11) NOT NULL,
   `WorkersLaid` int(11) NOT NULL,
-  `Percentage` float(1,6) NOT NULL,
+  `Percentage` float(2,6) NOT NULL,
    `Date` DATE NOT NULL,
    PRIMARY KEY (`LayoffId`),
-   CONSTRAINT `company_ibfk_1` FOREIGN KEY (`CompanyId`) REFERENCES `COMPANY` (`CompanyId`) ON UPDATE CASCADE
-   CONSTRAINT `company_ibfk_2` FOREIGN KEY (`LocationId`) REFERENCES `LOCATION` (`LocationId`) ON UPDATE CASCADE,
+   KEY `HeadquarterId` (`HeadquarterId`),
+
+   CONSTRAINT `layoff_ibfk_1` FOREIGN KEY (`HeadquarterId`) REFERENCES `HEADQUARTER` (`HeadquarterId`) ON UPDATE CASCADE
   )
   
-   DROP TABLE IF EXISTS `COUNTRY`;
+DROP TABLE IF EXISTS `COUNTRY`;
 
 CREATE TABLE COUNTRY (
   `CountryId` int(11) NOT NULL AUTO_INCREMENT,
@@ -40,22 +60,14 @@ CREATE TABLE COUNTRY (
    PRIMARY KEY (`CountryId`),
   )
   
-  DROP TABLE IF EXISTS `LOCATION`;
+DROP TABLE IF EXISTS `LOCATION`;
 
 CREATE TABLE LOCATION (
   `LocationId` int(11) NOT NULL AUTO_INCREMENT,
   `Name` varchar(255) NOT NULL,
   `CountryId` int(11),
    PRIMARY KEY (`LocationId`),
-   CONSTRAINT `company_ibfk_1` FOREIGN KEY (`CountryId`) REFERENCES `COUNTRY` (`CountryId`) ON UPDATE CASCADE
-  )
-  
-   DROP TABLE IF EXISTS `STAGE`;
-
-CREATE TABLE STAGE (
-  `StageId` int(11) NOT NULL AUTO_INCREMENT,
-  `Name` varchar(255) NOT NULL,
-   PRIMARY KEY (`StageId`),
+   CONSTRAINT `location_ibfk_1` FOREIGN KEY (`CountryId`) REFERENCES `COUNTRY` (`CountryId`) ON UPDATE CASCADE
   )
 
 DROP TABLE IF EXISTS `INDUSTRY`;
