5ddd454ef49f367ccd41c4c999fe87706232529d
ERJLM <99908664+ERJLM@users.noreply.github.com>
2022-12-23 00:58:00 +0100

    Update codigo.sql

--- SQL/codigo.sql
+++ SQL/codigo.sql
@@ -22,18 +22,19 @@ CREATE TABLE COMPANY_INDUSTRY (
    CONSTRAINT `company_industry_ibfk_2` FOREIGN KEY (`IndustryId`) REFERENCES `INDUSTRY` (`IndustyId`) ON UPDATE CASCADE  
   )
 
-DROP TABLE IF EXISTS `HEADQUARTER`;
+DROP TABLE IF EXISTS `BRANCH`;
 
-CREATE TABLE HEADQUARTER (
-  `HeadquarterId` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE BRANCH (
+  `BranchId` int(11) NOT NULL AUTO_INCREMENT,
   `Name` varchar(255) NOT NULL,
+  `Headquarter` bit NOT NULL DEFAULT (0),
   `CompanyId` int(11),
   `LocationId` int(11),
-   PRIMARY KEY (`HeadquarterId`),
+   PRIMARY KEY (`BranchId`),
    KEY `CompanyId` (`CompanyId`),
-   
-   CONSTRAINT `headquarter_ibfk_1` FOREIGN KEY (`CompanyId`) REFERENCES `COMPANY` (`CompanyId`) ON UPDATE CASCADE,
-   CONSTRAINT `headquarter_ibfk_2` FOREIGN KEY (`LocationId`) REFERENCES `LOCATION` (`LocationId`) ON UPDATE CASCADE    
+   KEY `LocationId` (`LocationId`),
+   CONSTRAINT `branch_ibfk_1` FOREIGN KEY (`CompanyId`) REFERENCES `COMPANY` (`CompanyId`) ON UPDATE CASCADE,
+   CONSTRAINT `branch_ibfk_2` FOREIGN KEY (`LocationId`) REFERENCES `LOCATION` (`LocationId`) ON UPDATE CASCADE    
   )
 
 
@@ -41,15 +42,15 @@ DROP TABLE IF EXISTS `LAYOFF`;
 
 CREATE TABLE LAYOFF(
   `LayoffId` int(11) AUTO_INCREMENT,
-  `HeadquarterId` int(11) NOT NULL,
+  `BranchId` int(11) NOT NULL,
   `FundsRaised` int(11) NOT NULL,
   `WorkersLaid` int(11) NOT NULL,
   `Percentage` float(2,6) NOT NULL,
    `Date` DATE NOT NULL,
    PRIMARY KEY (`LayoffId`),
-   KEY `HeadquarterId` (`HeadquarterId`),
+   KEY `BranchId` (`BranchId`),
 
-   CONSTRAINT `layoff_ibfk_1` FOREIGN KEY (`HeadquarterId`) REFERENCES `HEADQUARTER` (`HeadquarterId`) ON UPDATE CASCADE
+   CONSTRAINT `layoff_ibfk_1` FOREIGN KEY (`BranchId`) REFERENCES `BRANCH` (`BranchId`) ON UPDATE CASCADE
   )
   
 DROP TABLE IF EXISTS `COUNTRY`;
@@ -57,7 +58,7 @@ DROP TABLE IF EXISTS `COUNTRY`;
 CREATE TABLE COUNTRY (
   `CountryId` int(11) NOT NULL AUTO_INCREMENT,
   `Name` varchar(255) NOT NULL,
-   PRIMARY KEY (`CountryId`),
+   PRIMARY KEY (`CountryId`)
   )
   
 DROP TABLE IF EXISTS `LOCATION`;
@@ -75,5 +76,5 @@ DROP TABLE IF EXISTS `INDUSTRY`;
 CREATE TABLE INDUSTRY (
   `IndustryId` int(11) NOT NULL AUTO_INCREMENT,
   `Name` varchar(255) NOT NULL,
-   PRIMARY KEY (`IndustryId`),
+   PRIMARY KEY (`IndustryId`)
   )
